65089cd91f68b2f66a3ed83d8c552f0b23e6c4d3
ishtiaq786 <ahmad.ishtiaq990@gmail.com>
2018-12-18 21:31:49 +0500

    flask blog

---
+++ main.py
@@ -0,0 +1,62 @@
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+#import json
+from datetime import datetime
+
+#local_server = True
+#with open('configure.json','r') as c:
+    #params = json.load(c) ["params"]
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/codingthunder',
+#if(local_server):
+    #app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+#else
+    #app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+db = SQLAlchemy(app)
+
+
+class Contact(db.Model):
+
+
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(120), nullable=False)
+
+
+
+
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact", methods = ['GET', 'POST'])
+def contact():
+    #print(request.form)
+    if(request.method =='POST'):
+
+      '''Add entry to the database'''
+
+      name = request.form.get('name')
+      email = request.form.get('email')
+      phone = request.form.get('phone')
+      message = request.form.get('message')
+      entry = Contact(name=name, phone_num=phone, msg=message,
+                         date=datetime.now(), email=email)
+      db.session.add(entry)
+      db.session.commit();
+
+    return render_template('contact.html')
+
+app.run(debug=True)
---
+++ tut1.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    name = "ishtiaq"
+    return render_template('about.html', name = name)
+
+
+app.run(debug=True)
